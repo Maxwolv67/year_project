c9c218c17540140088647d32326ac0cd66b14b8c
Дима <knyazevdima05@gmail.com>
2020-12-06 16:19:13 +0300

    authentication_end

--- auth.py
+++ auth.py
@@ -1,8 +1,10 @@
 import sys
 
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QLineEdit
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
+
+import requests
 
 
 class Authorization(QDialog):
@@ -13,16 +15,41 @@ class Authorization(QDialog):
         self.setWindowTitle("Вход")
         self.initUI()
 
-
     def initUI(self):
         """инициализируем наш класс"""
         self.rejected.connect(self.to_main)
-        self.accepted.connect(self.to_main)
+        self.accepted.connect(self.find_in_db)
+
+    def find_in_db(self):
+        """Делает запрос и считывает данные из бд"""
+        self.login = self.login_label.text().strip()  # логин
+        self.password = self.password_label.text().strip()  # пароль
+        self.password_right = requests.get("http://localhost:8888/server.php", {'login': self.login, "mode": 'reg'})
+        self.password_right = self.password_right.text
+        if self.check_password():
+            self.hide()
+            self.par.show()
+        else:
+            self.show()
+
+    def check_password(self):
+        if not self.password_right:
+            self.error_label.setText("Пользователь не найден")
+            return False
+        if self.password == self.password_right:
+            self.par.login = self.login
+            name = requests.get("http://localhost:8888/server.php", {'login': self.login, "mode": 'name'})
+            self.par.name = name.text
+            self.par.after_init()
+            return True
+        else:
+            self.error_label.setText("Неверный пароль")
+            return False
+
 
     def to_main(self):
         self.par.show()
         self.hide()
-
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_F:
--- main.py
+++ main.py
@@ -20,20 +20,24 @@ class Main(QMainWindow):
 
         self.auth.clicked.connect(self.authorization)
 
+        self.name = ""
+        self.login = ""
+
         self.group_buttons_hand = QButtonGroup()
-        self.group_buttons_hand.buttonClicked.connect(self.print_names)
+        self.group_buttons_hand.buttonClicked.connect(self.which_button)
         self.fill_in_hand()
 
     def authorization(self):
+        """открывает окно входа"""
         self.hide()
         self.x = Authorization(self)
         self.x.show()
 
-    def in_db(self):
-        x = requests.get("http://localhost:8888/server.php", {'name': "Dima"})
-        print(x.text)
+    def after_init(self):
+        self.name_label.setText(self.name)
 
-    def print_names(self, button):
+    def which_button(self, button):
+        """Смотрит по названию, какая кнопка была нажата"""
         text = button.text()
         print(button.text())
         if text == "Освещение":
@@ -62,6 +66,7 @@ class Main(QMainWindow):
             self.layout_Area.addWidget(self.button)
 
     def keyPressEvent(self, event):
+        """Отслеживает нажатия клавиш"""
         if event.key() == Qt.Key_F:
             self.showFullScreen()
         if event.key() == Qt.Key_Escape:
